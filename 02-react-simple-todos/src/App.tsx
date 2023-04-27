b1d4481535c38c8a5c222052857dfde7e3732cf7
Kristopher Gray <kristopher.gray99@gmail.com>
2023-04-27 16:22:48 +0200

    li has unique id as well as key

--- 02-react-simple-todos/src/App.tsx
+++ 02-react-simple-todos/src/App.tsx
@@ -14,10 +14,26 @@ function App() {
 		{ title: 'Game', completed: false, id: 3 },
 		{ title: 'Repeat', completed: false, id: 4 },
 	])
+	const [newTodoTitle, setNewTodoTitle] = useState('')
 
 	const handleToggleTodo = (todo: Todo) => {
 		todo.completed = !todo.completed
 		setTodos([...todos])
+	}
+
+	const handleSubmitForm = (e: React.FormEvent) => {
+		e.preventDefault()
+
+		const newTodoId = todos.reduce((maxId, todo) => (todo.id > maxId) ? todo.id : maxId, 0) + 1
+
+		const newTodo: Todo = {
+			title: newTodoTitle,
+			completed: false,
+			id: newTodoId,
+		}
+
+		setTodos([...todos, newTodo])
+		setNewTodoTitle('')
 	}
 
 	return (
@@ -27,18 +43,22 @@ function App() {
 			<ul>
 				{todos.map(todo => (
 					<li
+						key={todo.id}
 						className={todo.completed ? 'completed' : ''}
 						onClick={() => handleToggleTodo(todo)}
-					>{todo.title}
+					>
+						{todo.title}
 					</li>
 				))}
 			</ul>
 
-			<form>
+			<form onSubmit={handleSubmitForm}>
 				<input
 					type="text"
 					placeholder='Create a todo...'
 					required
+					onChange={e => setNewTodoTitle(e.target.value)}
+					value={newTodoTitle}
 				/>
 
 				<button>Create</button>
