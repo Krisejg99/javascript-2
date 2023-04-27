e87e21ba9a9686db110f429670c3ca3ec15eceb3
Kristopher Gray <kristopher.gray99@gmail.com>
2023-04-27 15:48:55 +0200

    render list with completed status and able to toggle

--- 02-react-simple-todos/src/App.tsx
+++ 02-react-simple-todos/src/App.tsx
@@ -3,26 +3,46 @@ import './App.css'
 
 type Todo = {
 	title: string
+	completed: boolean
 	id: number
 }
 
-const todoList: Todo[] = [
-	{ title: 'Eat', id: 1 },
-	{ title: 'Sleep', id: 2 },
-	{ title: 'Game', id: 3 },
-	{ title: 'Repeat', id: 4 },
-]
-
 function App() {
-	const [todos, setTodos] = useState(todoList)
+	const [todos, setTodos] = useState([
+		{ title: 'Eat', completed: true, id: 1 },
+		{ title: 'Sleep', completed: true, id: 2 },
+		{ title: 'Game', completed: false, id: 3 },
+		{ title: 'Repeat', completed: false, id: 4 },
+	])
+
+	const handleToggleTodo = (todo: Todo) => {
+		todo.completed = !todo.completed
+		setTodos([...todos])
+	}
 
 	return (
 		<div className="App">
 			<h1>Todos</h1>
 
 			<ul>
-
+				{todos.map(todo => (
+					<li
+						className={todo.completed ? 'completed' : ''}
+						onClick={() => handleToggleTodo(todo)}
+					>{todo.title}
+					</li>
+				))}
 			</ul>
+
+			<form>
+				<input
+					type="text"
+					placeholder='Create a todo...'
+					required
+				/>
+
+				<button>Create</button>
+			</form>
 		</div>
 	)
 }
