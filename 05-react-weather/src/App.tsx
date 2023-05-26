a5e7ea56a9710c77f810f171a4e7f8b3989d1493
Kristopher Gray <kristopher.gray99@gmail.com>
2023-05-26 09:50:42 +0200

    show loading animation

--- 05-react-weather/src/App.tsx
+++ 05-react-weather/src/App.tsx
@@ -7,27 +7,27 @@ import Airplane from './assets/images/747.svg'
 import './assets/scss/App.scss'
 
 const App = () => {
-	const [currentWeather, setCurrentWeather] = useState<ICurrentWeather>()
+	const [currentWeather, setCurrentWeather] = useState<ICurrentWeather | null>(null)
+	const [loading, setLoading] = useState(false)
 
 	const handleSearch = async (city: string) => {
-		const weather = await getCurrentWeather(city)
-		if (!weather) {
+		setCurrentWeather(null)
+		setLoading(true)
 
-		}
+		const weather = await getCurrentWeather(city)
 
 		setCurrentWeather(weather)
+		setLoading(false)
 	}
-
 
 	return (
 		<div id="app" className="container">
-			<SearchCity
-				onSearch={handleSearch}
-			/>
+			<SearchCity onSearch={handleSearch} />
 
-			{currentWeather && <Forecast
-				currentWeather={currentWeather}
-			/>}
+			{loading && <img src={Airplane} alt="Airplane animation" className='img-fluid py-5 w-100' />}
+
+			{currentWeather &&
+				<Forecast currentWeather={currentWeather} />}
 		</div>
 	)
 }
