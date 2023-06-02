9a803b7c9d4876affefd655b36b103111b6f719b
Kristopher Gray <kristopher.gray99@gmail.com>
2023-06-02 14:16:29 +0200

    Initialized routes in App, a HomePage, a Navigation component, a NotFound page and a SearchPage

--- 07-hacker-news/src/App.tsx
+++ 07-hacker-news/src/App.tsx
@@ -1,10 +1,24 @@
 import './assets/scss/App.scss'
+import Container from 'react-bootstrap/Container'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import SearchPage from './pages/SearchPage'
+import Navigation from './components/Navigation'
+import NotFound from './pages/NotFound'
 
 const App = () => {
 	return (
-		<>
-			<h1>Hacker News Search</h1>
-		</>
+		<div id="App">
+			<Navigation />
+
+			<Container className='py-3'>
+				<Routes>
+					<Route path='/' element={<HomePage />} />
+					<Route path='/search' element={<SearchPage />} />
+					<Route path='*' element={<NotFound />} />
+				</Routes>
+			</Container>
+		</div>
 	)
 }
 
---
+++ 07-hacker-news/src/pages/HomePage.tsx
@@ -0,0 +1,16 @@
+import Button from 'react-bootstrap/Button'
+import { Link } from 'react-router-dom'
+
+const HomePage = () => {
+	return (
+		<div>
+			<h1>Welcome</h1>
+
+			<Link to='/search'>
+				<Button>Search</Button>
+			</Link>
+		</div>
+	)
+}
+
+export default HomePage
