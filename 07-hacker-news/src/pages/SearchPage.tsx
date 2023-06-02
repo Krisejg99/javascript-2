1043165a5db67596f291101eeabdc274eefc3ab4
Kristopher Gray <kristopher.gray99@gmail.com>
2023-06-02 14:54:50 +0200

    react-bootstrap From used and temporary false loading

--- 07-hacker-news/src/pages/SearchPage.tsx
+++ 07-hacker-news/src/pages/SearchPage.tsx
@@ -1,6 +1,28 @@
+import { useEffect, useRef, useState } from 'react'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import ListGroup from 'react-bootstrap/ListGroup'
+
 const SearchPage = () => {
 	return (
-		<h1>Search</h1>
+		<>
+			<h1>Search 🔎</h1>
+
+			<Form className='mb-3'>
+				<Form.Group className="mb-3" controlId="formBasicEmail">
+					<Form.Control
+						type="text"
+						placeholder="Enter your search query"
+					/>
+				</Form.Group>
+
+				<div className="d-flex justify-content-end">
+					<Button variant="success" type="submit">Search</Button>
+				</div>
+			</Form>
+
+			{false && <p>Loading...</p>}
+		</>
 	)
 }
 
