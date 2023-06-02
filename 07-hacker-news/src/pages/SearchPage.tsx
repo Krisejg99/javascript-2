dd13866cc6047e89466600fba33b09ac08a9bcc5
Kristopher Gray <kristopher.gray99@gmail.com>
2023-06-02 15:28:37 +0200

    handleSubmit, loading and error states

--- 07-hacker-news/src/pages/SearchPage.tsx
+++ 07-hacker-news/src/pages/SearchPage.tsx
@@ -1,27 +1,57 @@
-import { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import ListGroup from 'react-bootstrap/ListGroup'
 
 const SearchPage = () => {
+	const [loading, setLoading] = useState(false)
+	const [error, setError] = useState<string | null>(null)
+	const [searchInput, setSearchInput] = useState('')
+
+
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault()
+
+		if (!searchInput.trim().length) return
+
+		setLoading(true)
+
+		try {
+
+		}
+		catch (err: any) {
+			setError(err.message)
+		}
+
+		setLoading(false)
+	}
+
 	return (
 		<>
 			<h1>Search 🔎</h1>
 
-			<Form className='mb-3'>
+			<Form
+				className='mb-3'
+				onSubmit={handleSubmit}
+			>
 				<Form.Group className="mb-3" controlId="formBasicEmail">
 					<Form.Control
 						type="text"
 						placeholder="Enter your search query"
+						required
+						value={searchInput}
+						onChange={e => setSearchInput(e.target.value)}
 					/>
 				</Form.Group>
 
 				<div className="d-flex justify-content-end">
-					<Button variant="success" type="submit">Search</Button>
+					<Button variant="success" type="submit" disabled={!searchInput.trim().length}>Search</Button>
 				</div>
 			</Form>
 
 			{false && <p>Loading...</p>}
+
+			{false && <p>Error!</p>}
 
 			{true && (
 				<div id="search-result">
