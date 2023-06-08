34d7eadc92eb8ecfae6e4468a432dcb3b0e9503b
Kristopher Gray <kristopher.gray99@gmail.com>
2023-06-08 10:51:40 +0200

    ThemeContextProvider provieds a ThemeContext to be able to reach it in HomePage for light and dark mode

---
+++ 07-hacker-news/src/contexts/ThemeContextProvider.tsx
@@ -0,0 +1,27 @@
+import { createContext, useState } from 'react'
+
+type ThemeContextType = {
+	isDarkMode: boolean
+}
+
+export const ThemeContext = createContext<ThemeContextType>({ isDarkMode: false })
+
+interface IProps {
+	children: React.ReactNode
+}
+
+const ThemeProvider: React.FC<IProps> = ({ children }) => {
+	const [isDarkMode, setIsDarkMode] = useState<boolean>(false)
+
+	const toggleTheme = () => {
+		setIsDarkMode(!isDarkMode)
+	}
+
+	return (
+		<ThemeContext.Provider value={{ isDarkMode }}>
+			{children}
+		</ThemeContext.Provider>
+	)
+}
+
+export default ThemeProvider
--- 07-hacker-news/src/pages/HomePage.tsx
+++ 07-hacker-news/src/pages/HomePage.tsx
@@ -1,10 +1,16 @@
+import { useContext } from 'react'
+import { ThemeContext } from '../contexts/ThemeContextProvider'
 import Button from 'react-bootstrap/Button'
 import { Link } from 'react-router-dom'
 
 const HomePage = () => {
+	const { isDarkMode } = useContext(ThemeContext)
+
 	return (
 		<div>
 			<h1>Welcome</h1>
+
+			<p>Your theme is: {isDarkMode ? 'dark' : 'light'}</p>
 
 			<Link to='/search'>
 				<Button>Search</Button>
