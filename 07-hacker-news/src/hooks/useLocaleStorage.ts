4f44c2bcae35bbc1ceefd050f9995821897fcf1d
Kristopher Gray <kristopher.gray99@gmail.com>
2023-06-13 09:58:39 +0200

    fixed localStorage hook

--- 07-hacker-news/src/hooks/useLocaleStorage.ts
+++ 07-hacker-news/src/hooks/useLocaleStorage.ts
@@ -1,22 +1,24 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 const useLocaleStorage = <T>(key: string, defaultValue: T) => {
 	const [storedValue, setStoredValue] = useState<T>(() => {
 		const value = localStorage.getItem(key)
 
 		return value
-			? JSON.parse(value)
+			? JSON.parse(value) as T || defaultValue
 			: defaultValue
 	})
 
-	useEffect(() => {
-		localStorage.setItem(key, JSON.stringify(storedValue))
-	}, [key, storedValue])
+	const setValue = (value: T) => {
+		setStoredValue(value)
 
-  return [
-	storedValue,
-	setStoredValue,
-  ]
+		localStorage.setItem(key, JSON.stringify(value))
+	}
+
+	return [
+		storedValue,
+		setValue,
+	] as const
 }
 
 export default useLocaleStorage
