1a64e8f09fd18695860b34a128d473c84df25c6f
Kristopher Gray <kristopher.gray99@gmail.com>
2023-08-16 16:12:37 +0200

    clicking the buttons either refetches or changes the breedId which gets that breed or brings back the cashed picture.

--- 10-react-query/src/pages/RandomCatPage.tsx
+++ 10-react-query/src/pages/RandomCatPage.tsx
@@ -1,12 +1,14 @@
+import { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { getRandomCatImage } from '../services/TheCatAPI'
 import { Image, Button, Alert } from 'react-bootstrap'
 import CatSpinner from '../components/CatSpinner'
 
 const RandomCatPage = () => {
-	const { data, refetch, isLoading, isError, error, isStale, isFetching } = useQuery({
-		queryKey: ['random-cat'],
-		queryFn: getRandomCatImage
+	const [breedId, setBreedId] = useState('')
+	const { data, isLoading, isError, error, isStale, isFetching, refetch } = useQuery({
+		queryKey: !breedId ? ['random-cat'] : ['random-cat', breedId],
+		queryFn: () => getRandomCatImage(breedId),
 	})
 
 	if (error) {
@@ -15,19 +17,27 @@ const RandomCatPage = () => {
 
 	return (
 		<>
-			<h1>Random Cat</h1>
-
 			<span>isLoading: {isLoading ? 'true' : 'false'}</span><br />
 			<span>isFetching: {isFetching ? 'true' : 'false'}</span><br />
 			<span>isStale: {isStale ? 'true' : 'false'}</span><br />
-			<span>isError: {isError ? 'true' : 'false'}</span><br /><br />
+			<span>isError: {isError ? 'true' : 'false'}</span><br />
+			<span>error: {error ? 'true' : 'false'}</span><br />
+
+			<h1>Find your cat:</h1>
 
 			<Button
-				onClick={() => refetch()}
+				onClick={() => breedId === '' ? refetch() : setBreedId('')}
 				disabled={isFetching}
-				className='mb-3'
+				className='mb-3 me-3'
 			>
-				{isFetching ? 'Finding new cat...' : 'Refresh cat'}
+				{isFetching ? 'Finding...' : 'Random'}
+			</Button>
+			<Button
+				onClick={() => breedId === 'sphy' ? refetch() : setBreedId('sphy')}
+				disabled={isFetching}
+				className='mb-3 me-3'
+			>
+				{isFetching ? 'Finding...' : 'Sphynx'}
 			</Button>
 
 			{isFetching && <CatSpinner />}
