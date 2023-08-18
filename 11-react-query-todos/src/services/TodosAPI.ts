f11db6da288e7fead9a1e4442fcd185c948b8e4e
Kristopher Gray <kristopher.gray99@gmail.com>
2023-08-18 13:50:57 +0200

    created an instace of axios, a fake delay and a generic get function

--- 11-react-query-todos/src/services/TodosAPI.ts
+++ 11-react-query-todos/src/services/TodosAPI.ts
@@ -4,22 +4,38 @@
 import axios from 'axios'
 import { PartialTodo, Todo } from '../types'
 
-const BASE_URL = 'http://localhost:3000'
+const FAKE_DELAY = 1000 * 0
+
+const instance = axios.create({
+	baseURL: 'http://localhost:3000',
+	timeout: 10000,
+	headers: {
+		"Content-Type": "application/json",
+		"Accept": "application/json"
+	},
+})
+
+const get = async <T>(endpoint: string) => {
+	const res = await instance.get<T>(endpoint)
+
+	// Fake delay
+	!!FAKE_DELAY && await new Promise(r => setTimeout(r, FAKE_DELAY))
+
+	return res.data
+}
 
 /**
  * Get all todos
  */
 export const getTodos = async () => {
-	const res = await axios.get(`${BASE_URL}/todos`)
-	return res.data as Todo[]
+	return await get<Todo[]>('todos')
 }
 
 /**
  * Get a single todo
  */
 export const getTodo = async (todoId: number) => {
-	const res = await axios.get(`${BASE_URL}/todos/${todoId}`)
-	return res.data as Todo
+	return await get<Todo>(`/todos/${todoId}`)
 }
 
 /**
@@ -28,7 +44,7 @@ export const getTodo = async (todoId: number) => {
  * @param data Object with properties and values for the new todo
  */
 export const createTodo = async (todo: Todo) => {
-	const res = await axios.post(`${BASE_URL}/todos`, todo)
+	const res = await instance.post(`/todos`, todo)
 	return res.data as Todo
 }
 
@@ -39,7 +55,7 @@ export const createTodo = async (todo: Todo) => {
  * @param data Data to update todo with
  */
 export const updateTodo = async (todoId: number, data: PartialTodo) => {
-	const res = await axios.patch(`${BASE_URL}/todos/${todoId}`, data)
+	const res = await instance.patch(`/todos/${todoId}`, data)
 	return res.data as Todo
 }
 
@@ -49,6 +65,6 @@ export const updateTodo = async (todoId: number, data: PartialTodo) => {
  * @param todo_id Todo to delete
  */
 export const deleteTodo = async (todoId: number) => {
-	const res = await axios.delete(`${BASE_URL}/todos/${todoId}`)
+	const res = await instance.delete(`/todos/${todoId}`)
 	return res.data
 }
