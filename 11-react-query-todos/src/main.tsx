9e2dcc8fb7ac140eb05ad3edeeaac9f66890b656
Kristopher Gray <kristopher.gray99@gmail.com>
2023-08-18 14:21:02 +0200

    queryClient, defauldOptions, queries set

--- 11-react-query-todos/src/main.tsx
+++ 11-react-query-todos/src/main.tsx
@@ -5,7 +5,14 @@ import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: 1000 * 60 * 5,
+			cacheTime: 1000 * 60 * 60,
+		}
+	}
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
