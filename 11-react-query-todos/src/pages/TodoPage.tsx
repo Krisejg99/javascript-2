f903f27af947b579c683dd9c104eb74911311ed9
Kristopher Gray <kristopher.gray99@gmail.com>
2023-08-18 14:22:25 +0200

    get the single todos data using react-query

--- 11-react-query-todos/src/pages/TodoPage.tsx
+++ 11-react-query-todos/src/pages/TodoPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { Link, useLocation, useNavigate, useParams } from "react-router-dom"
 import { Todo } from "../types"
 import * as TodosAPI from '../services/TodosAPI'
@@ -6,33 +6,19 @@ import Button from 'react-bootstrap/Button'
 import Alert from 'react-bootstrap/Alert'
 import ConfirmationModal from "../components/ConfirmationModal"
 import AutoDismissingAlert from "../components/AutoDismissingAlert"
+import { useQuery } from '@tanstack/react-query'
 
 const TodoPage = () => {
-	const [todo, setTodo] = useState<Todo | null>(null)
-	const [loading, setLoading] = useState(false)
-	const [error, setError] = useState<String | null>(null)
-	const [showConfirmDelete, setShowConfirmDelete] = useState(false)
-
 	const { id } = useParams()
 	const todoId = Number(id)
+	const { data: todo, isError, refetch } = useQuery({
+		queryKey: ['todo', todoId],
+		queryFn: () => TodosAPI.getTodo(todoId),
+	})
+	const [showConfirmDelete, setShowConfirmDelete] = useState(false)
 
 	const navigate = useNavigate()
 	const location = useLocation()
-
-	const getTodo = async (id: number) => {
-		setError(null)
-		setLoading(true)
-
-		try {
-			const todo = await TodosAPI.getTodo(id)
-			setTodo(todo)
-		}
-		catch (err: any) {
-			setError(err.message)
-		}
-
-		setLoading(false)
-	}
 
 	const handleDeleteTodo = async (todo: Todo) => {
 		if (!todo.id) return
@@ -47,23 +33,15 @@ const TodoPage = () => {
 		})
 	}
 
-	const handleToggleTodo = async (todo: Todo) => {
-		if (!todo.id) return
+	// const handleToggleTodo = async (todo: Todo) => {
+	// 	if (!todo.id) return
 
-		const updatedTodo = await TodosAPI.updateTodo(todo.id, {
-			completed: !todo.completed
-		})
+	// 	const updatedTodo = await TodosAPI.updateTodo(todo.id, {
+	// 		completed: !todo.completed
+	// 	})
 
-		setTodo(updatedTodo)
-	}
-
-	useEffect(() => {
-		if (typeof todoId !== "number") {
-			return
-		}
-
-		getTodo(todoId)
-	}, [todoId])
+	// 	setTodo(updatedTodo)
+	// }
 
 	// useEffect(() => {
 	// 	if (location.state?.message) {
@@ -73,19 +51,14 @@ const TodoPage = () => {
 	// 	}
 	// }, [])
 
-	if (error) {
+	if (isError) {
 		return (
 			<Alert variant="danger">
 				<h1>Something went wrong!</h1>
-				<p>{error}</p>
 
-				<Button variant="primary" onClick={() => getTodo(todoId)}>Try again</Button>
+				<Button variant="primary" onClick={() => refetch()}>Try again</Button>
 			</Alert >
 		)
-	}
-
-	if (loading || !todo) {
-		return (<p>Loading...</p>)
 	}
 
 	return (
@@ -99,28 +72,32 @@ const TodoPage = () => {
 				</AutoDismissingAlert>
 			)}
 
-			<div>{todo.title}</div>
+			{todo && (
+				<>
+					<div>{todo.title}</div>
 
-			<p><strong>Status:</strong> {todo.completed ? 'Completed' : 'Not completed'}</p>
+					<p><strong>Status:</strong> {todo.completed ? 'Completed' : 'Not completed'}</p>
 
-			<div className="buttons mb-3">
-				<Button variant="success" onClick={() => handleToggleTodo(todo)}>Toggle</Button>
+					<div className="buttons mb-3">
+						{/* <Button variant="success" onClick={() => handleToggleTodo(todo)}>Toggle</Button> */}
 
-				<Link to={`/todos/${todoId}/edit`}>
-					<Button variant="warning">Edit</Button>
-				</Link>
+						<Link to={`/todos/${todoId}/edit`}>
+							<Button variant="warning">Edit</Button>
+						</Link>
 
-				<Button variant="danger" onClick={() => setShowConfirmDelete(true)}>Delete</Button>
-			</div>
+						<Button variant="danger" onClick={() => setShowConfirmDelete(true)}>Delete</Button>
+					</div>
 
 
-			<ConfirmationModal
-				show={showConfirmDelete}
-				onConfirm={() => handleDeleteTodo(todo)}
-				onCancel={() => setShowConfirmDelete(false)}
-			>
-				Delete this todo
-			</ConfirmationModal >
+					<ConfirmationModal
+						show={showConfirmDelete}
+						onConfirm={() => handleDeleteTodo(todo)}
+						onCancel={() => setShowConfirmDelete(false)}
+					>
+						Delete this todo
+					</ConfirmationModal >
+				</>
+			)}
 
 
 
