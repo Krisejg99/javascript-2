b4359699983961ea008668d21d52eb0cd46c33c5
Kristopher Gray <kristopher.gray99@gmail.com>
2023-08-25 14:18:11 +0200

    forgot to add file to last commit.

--- 11-react-query-todos/src/hooks/useTodos.ts
+++ 11-react-query-todos/src/hooks/useTodos.ts
@@ -1,11 +1,15 @@
 import { useQuery } from "@tanstack/react-query"
 import { getTodos } from "../services/TodosAPI"
+import sortTodos from "../functions/sortTodos"
 
 const useTodos = () => {
-  return useQuery({
-	queryKey: ['todos'],
-	queryFn: getTodos,
-  })
+	return useQuery({
+		queryKey: ['todos'],
+		queryFn: async () => {
+			const todos = await getTodos()
+			return sortTodos(todos)
+		},
+	})
 }
 
 export default useTodos
