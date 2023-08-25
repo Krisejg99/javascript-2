a0be730ebd4e36f1dc38fa79748f57b538e6c223
Kristopher Gray <kristopher.gray99@gmail.com>
2023-08-25 14:12:42 +0200

    hook for getting todos and hook for getting single todo

---
+++ 11-react-query-todos/src/hooks/useTodo.ts
@@ -0,0 +1,12 @@
+import { useQuery } from "@tanstack/react-query"
+import { getTodo } from "../services/TodosAPI"
+
+const useTodo = (todoId: number, enabled = true) => {
+	return useQuery({
+		queryKey: ['todo', { id: todoId }],
+		queryFn: () => getTodo(todoId),
+		enabled,
+	})
+}
+
+export default useTodo
