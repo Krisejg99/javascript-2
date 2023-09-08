758ed531b4f4e8f1861bae7455f7c807fb44c487
Kristopher Gray <kristopher.gray99@gmail.com>
2023-09-08 13:04:43 +0200

    seding in orderBy to firebase/firestore to get sorted todos

--- 13-firebase-todos/src/hooks/useStreamCollection.ts
+++ 13-firebase-todos/src/hooks/useStreamCollection.ts
@@ -1,12 +1,17 @@
-import { CollectionReference, onSnapshot } from "firebase/firestore"
+import { CollectionReference, QueryConstraint, onSnapshot, query } from "firebase/firestore"
 import { useEffect, useState } from "react"
 
-const useStreamCollection = <T>(colRef: CollectionReference<T>) => {
+const useStreamCollection = <T>(
+	colRef: CollectionReference<T>,
+	...queryConstraints: QueryConstraint[]
+) => {
 	const [data, setData] = useState<T[] | null>(null)
 	const [loading, setLoading] = useState(true)
 
 	useEffect(() => {
-		const unsubscribe = onSnapshot(colRef, (snapshot) => {
+		const queryRef = query(colRef, ...queryConstraints)
+
+		const unsubscribe = onSnapshot(queryRef, (snapshot) => {
 			const data: T[] = snapshot.docs.map(doc => {
 				return {
 					...doc.data(),
