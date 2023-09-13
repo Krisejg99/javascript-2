9684cb8f254ba50ea685c995abf85b0dbf3a03ae
Kristopher Gray <kristopher.gray99@gmail.com>
2023-09-13 11:02:22 +0200

    sing up error handled nicely...

--- 13-firebase-todos/src/pages/SignUpPage.tsx
+++ 13-firebase-todos/src/pages/SignUpPage.tsx
@@ -1,27 +1,49 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 import Col from 'react-bootstrap/Col'
 import Form from 'react-bootstrap/Form'
 import Row from 'react-bootstrap/Row'
+import Alert from 'react-bootstrap/Alert'
 import { useForm, SubmitHandler } from 'react-hook-form'
-import { Link } from 'react-router-dom'
-import { SignUpSchema, signUpSchema } from '../schemas/SignUpSchema'
-import { zodResolver } from '@hookform/resolvers/zod'
+import { Link, useNavigate } from 'react-router-dom'
+import { SignUpSchema } from '../schemas/SignUpSchema'
 import useAuth from '../hooks/useAuth'
+import { FirebaseError } from 'firebase/app'
 
 const SignUpPage = () => {
+	const [errorMessage, setErrorMessage] = useState<string | null>(null)
+	const [loading, setLoading] = useState(false)
 	const { register, handleSubmit, formState: { errors }, watch } = useForm<SignUpSchema>({
 		// resolver: zodResolver(signUpSchema)
 	})
 
 	const { signUp } = useAuth()
 
+	const navigate = useNavigate()
+
 	const passwordRef = useRef('')
 	passwordRef.current = watch('password')
 
-	const submitForm: SubmitHandler<SignUpSchema> = async (data: SignUpSchema) => {
-		await signUp(data.email, data.password)
+	const onSignUp: SubmitHandler<SignUpSchema> = async (data: SignUpSchema) => {
+		setErrorMessage(null)
+
+		try {
+			setLoading(true)
+			await signUp(data.email, data.password)
+
+			navigate('/')
+		}
+		catch (error) {
+			if (error instanceof FirebaseError) {
+				setErrorMessage(error.message)
+			}
+			else {
+				setErrorMessage('Something went wrong. Have you tried turning it off and back on again?')
+			}
+
+			setLoading(false)
+		}
 	}
 
 	return (
@@ -31,7 +53,9 @@ const SignUpPage = () => {
 					<Card.Body>
 						<Card.Title className='mb-3'>Sign Up</Card.Title>
 
-						<Form onSubmit={handleSubmit(submitForm)}>
+						{errorMessage && <Alert variant='danger'>{errorMessage}</Alert>}
+
+						<Form onSubmit={handleSubmit(onSignUp)}>
 							<Form.Group controlId='email'>
 								<Form.Label>Email</Form.Label>
 								<Form.Control
@@ -68,7 +92,10 @@ const SignUpPage = () => {
 								{errors.confirmPassword && <span className='text-danger'>{errors.confirmPassword.message || 'Invalid password confirmation'}</span>}
 							</Form.Group>
 
-							<Button type='submit'>Sign Up</Button>
+							<Button
+								type='submit'
+								disabled={loading}
+							>Sign Up</Button>
 						</Form>
 					</Card.Body>
 				</Card>
