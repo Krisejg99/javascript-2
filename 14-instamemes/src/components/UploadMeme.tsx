0013af70ff500311f767f40a20102dac82542867
Kristopher Gray <kristopher.gray99@gmail.com>
2023-09-15 13:36:16 +0200

    only accept certain files, maxSize and maxFiles

--- 14-instamemes/src/components/UploadMeme.tsx
+++ 14-instamemes/src/components/UploadMeme.tsx
@@ -1,23 +1,42 @@
+import classNames from 'classnames'
 import { useCallback } from 'react'
 import { useDropzone } from 'react-dropzone'
+import { toast } from 'react-toastify'
 
 const UploadMeme = () => {
 	const onDrop = useCallback((acceptedFiles: File[]) => {
+		if (!acceptedFiles.length) return toast.error("I don't like you anymore...")
+
 		console.log('acceptedFiles', acceptedFiles)
 	}, [])
 
-	const { getRootProps, getInputProps, isDragActive } = useDropzone({
+	const { getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject } = useDropzone({
 		onDrop: onDrop,
+		maxFiles: 1,
+		maxSize: 4 * 1024 * 1024, // 4 MB
+		accept: {
+			'image/gif': [],
+			'image/jpeg': [],
+			'image/png': [],
+			'image/webp': [],
+		}
+	})
+
+	const dropzoneWrapperClasses = classNames({
+		'drag-accept': isDragAccept,
+		'drag-reject': isDragReject,
 	})
 
 	return (
-		<div {...getRootProps()} id='dropzone-wrapper'>
+		<div {...getRootProps()} id='dropzone-wrapper' className={dropzoneWrapperClasses}>
 			<input {...getInputProps()} />
 
 			<div className='indicator'>
-				{isDragActive
-					? <p>Drop it like it's hot!</p>
-					: <p>Your memes belong to me!!!</p>
+				{!isDragActive
+					? <p>Your memes belong to me!!!</p>
+					: isDragAccept
+						? <p>Drop it like it's hot!</p>
+						: <p>No no, not that...</p>
 				}
 			</div>
 		</div>
