54ed24cc48c33655047ee5346497302e6bbfb8ee
Kristopher Gray <kristopher.gray99@gmail.com>
2023-09-19 12:03:19 +0200

    ReducerCounter using usereducer

--- 15-state-management/src/components/ReducerCounter.tsx
+++ 15-state-management/src/components/ReducerCounter.tsx
@@ -1,14 +1,66 @@
 import Button from 'react-bootstrap/Button'
+import { Reducer, useReducer } from 'react'
+
+enum PointsActionTypes {
+	INCREMENT = "increment",
+	DECREMENT = "decrement",
+}
+
+type PointsState = {
+	points: number
+}
+
+type PointsAction = {
+	type: PointsActionTypes
+}
+
+const initialState: PointsState = {
+	points: 0
+}
+
+const pointsReducer = (state: PointsState, action: PointsAction) => {
+	switch (action.type) {
+		case PointsActionTypes.DECREMENT:
+			return {
+				...state,
+				points: state.points - 1
+			}
+
+		case PointsActionTypes.INCREMENT:
+			return {
+				...state,
+				points: state.points + 1
+			}
+
+		default:
+			return state
+	}
+}
+
+const decreasePoints = () => {
+	return { type: PointsActionTypes.DECREMENT }
+}
+
+const increasePoints = () => {
+	return { type: PointsActionTypes.INCREMENT }
+}
 
 const ReducerCounter = () => {
+	const [state, dispatch] = useReducer<Reducer<PointsState, PointsAction>>(pointsReducer, initialState)
 
 	return (
 		<div className="counter">
-			<Button variant="warning" onClick={() => null}>-</Button>
+			<Button
+				variant="warning"
+				onClick={() => dispatch(decreasePoints())}
+			>-</Button>
 
-			<span className="points">{null}</span>
+			<span className="points">{state.points}</span>
 
-			<Button variant="success" onClick={() => null}>+</Button>
+			<Button
+				variant="success"
+				onClick={() => dispatch(increasePoints())}
+			>+</Button>
 		</div>
 	)
 }
