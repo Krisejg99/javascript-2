f80040cc4204c3ebb57d3fc1bf3a768e401fd18a
Kristopher Gray <kristopher.gray99@gmail.com>
2023-09-21 11:13:15 +0200

    added accountSlice and added it to the store

--- 16-redux-toolkit/src/app/store.ts
+++ 16-redux-toolkit/src/app/store.ts
@@ -1,5 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { configureStore } from '@reduxjs/toolkit'
+import accountReducer from '../features/account/accountSlice'
 
 export const store = configureStore({
-	reducer: {},
+	reducer: {
+		account: accountReducer
+	},
 })
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
